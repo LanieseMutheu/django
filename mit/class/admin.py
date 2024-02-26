a2a9037d422fb719e14984e6168a366a3e7fdb28
Laniese <laniesemutheu@gmail.com>
2024-02-26 10:53:16 +0300

    sasa

--- mit/class/admin.py
+++ mit/class/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . models import student
+from . models import teacher
 # Register your models here.
+
+admin.site.register(student)
+
+admin.site.register(teacher)
--- mit/class/models.py
+++ mit/class/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class student(models.Model):
+    name = models.CharField(max_length=20, null=False)
+    phone = models.IntegerField()
+    email = models.EmailField(blank= False)
+
+    def __str__(self):
+        return self.name
+
+class teacher(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField(blank= False)
+    tscnumber = models.IntegerField()
+    phone = models.IntegerField()
+
+    def __str__(self):
+        return self.name
